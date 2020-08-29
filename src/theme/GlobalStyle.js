58385f2c8f31ff303d2550d6dc874c468ceadae1
nightspite <n16h75p173@gmail.com>
2020-08-30 01:47:14 +0200

    ❤️ lint, prettier, husky, lint-staged and styled components config

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -2,17 +2,17 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Montserrat:300,600');
-  
+
   *, *::before, *::after {
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-  
+
   html {
-    font-size: 62.5%; 
+    font-size: 62.5%;
   }
-  
+
   body {
     font-size: 1.6rem;
     font-family: "Montserrat", sans-serif;
