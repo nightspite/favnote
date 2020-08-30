10befdd6be8eaaa06fd07a2e2648514e37abd4c4
nightspite <n16h75p173@gmail.com>
2020-08-30 15:49:01 +0200

    👍 added favicon and GlobalStyles

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -5,8 +5,7 @@ import GlobalStyle from 'theme/GlobalStyle';
 const Root = () => (
   <div>
     <GlobalStyle />
-    <h1>Hello Roman</h1>
-    <Button>Close / Save</Button>
+    <Button width="200px">Close / Save</Button>
     <Button secondary>Remove</Button>
   </div>
 );
