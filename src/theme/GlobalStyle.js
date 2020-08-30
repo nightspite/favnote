24231c37bc2ece4b50988951ca83038a2f1b3a41
nightspite <we4rpl2@gmail.com>
2020-08-30 15:49:01 +0200

    👍 added favicon and GlobalStyles

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,9 +1,9 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,600');
-
-  *, *::before, *::after {
+const GlobalStyle = createGlobalStyle`${css`
+  *,
+  *::before,
+  *::after {
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
@@ -15,8 +15,8 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-size: 1.6rem;
-    font-family: "Montserrat", sans-serif;
+    font-family: 'Montserrat', sans-serif;
   }
-`;
+`}`;
 
 export default GlobalStyle;
