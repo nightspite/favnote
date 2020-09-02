c58bd679b15851b1d1830fd4129b320de8d6211d
nightspite <n16h75p173@gmail.com>
2020-09-02 11:20:06 +0200

    💣 created ButtonIcon component

--- src/components/atoms/ButtonIcon/Button.stories.js
+++ src/components/atoms/ButtonIcon/Button.stories.js
@@ -1,1 +1,29 @@
-// ButtonIcon story// ButtonIcon story
+import React from 'react';
+import styled from 'styled-components';
+import bulbIcon from 'assets/icons/bulb.svg';
+import twitterIcon from 'assets/icons/twitter.svg';
+import penIcon from 'assets/icons/pen.svg';
+import logoutIcon from 'assets/icons/logout.svg';
+import plusIcon from 'assets/icons/plus.svg';
+import ButtonIcon from './ButtonIcon';
+
+const YellowBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 150px;
+  background-color: ${({ theme }) => theme.primary};
+`;
+
+export default {
+  title: 'Components/ButtonIcon',
+  component: ButtonIcon,
+  decorators: [story => <YellowBackground>{story()}</YellowBackground>],
+};
+
+export const Bulb = () => <ButtonIcon active icon={bulbIcon}></ButtonIcon>;
+export const Twitter = () => <ButtonIcon icon={twitterIcon}></ButtonIcon>;
+export const Pen = () => <ButtonIcon icon={penIcon}></ButtonIcon>;
+export const Logout = () => <ButtonIcon icon={logoutIcon}></ButtonIcon>;
+export const Plus = () => <ButtonIcon icon={plusIcon}></ButtonIcon>;
--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components';
-import magnifierIcon from 'assets/magnifier.svg';
+import magnifierIcon from 'assets/icons/magnifier.svg';
 
 const Input = styled.input`
   padding: 15px 30px;
