51fdc5352c0202c3b224dfc027dc53803289f859
nightspite <n16h75p173@gmail.com>
2020-09-02 13:42:34 +0200

    💣 createad a Card component as a molecule

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src/theme/mainTheme';
-
-addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+export const decorators = [story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>];
