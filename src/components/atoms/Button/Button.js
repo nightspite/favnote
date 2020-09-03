8e25e847c0f695688c50f03fc315cc62808b36b5
nightspite <we4rpl2@gmail.com>
2020-09-03 20:19:19 +0200

    💼 more advanced UI and created a readme file

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -2,7 +2,6 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-
   background-color: ${({ theme }) => theme.note};
   /* background-color: ${({ color }) => color};  //for stories knobs */
   width: 220px;
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import MainTemplate from 'templates/MainTemplate';
 import Notes from 'views/Notes';
 import Twitters from 'views/Twitters';
 import Articles from 'views/Articles';
+import DetailsPage from 'views/DetailsPage';
 
 const Root = () => (
   <div>
     <BrowserRouter>
       <MainTemplate>
         <Switch>
-          <Route exact path="/" component={Notes} />
-          <Route path="/twitters" component={Twitters} />
-          <Route path="/articles" component={Articles} />
+          <Route exact path="/" render={() => <Redirect to="/notes" />} />
+          <Route exact path="/notes" component={Notes} />
+          <Route path="/notes/:id" component={DetailsPage} />
+          <Route exact path="/twitters" component={Twitters} />
+          <Route path="/twitters/:id" component={DetailsPage} />
+          <Route exact path="/articles" component={Articles} />
+          <Route path="/articles/:id" component={DetailsPage} />
         </Switch>
       </MainTemplate>
     </BrowserRouter>
