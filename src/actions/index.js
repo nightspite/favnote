62701d3cac616b82eed667f4ba6acea02ae51d32
nightspite <we4rpl2@gmail.com>
2020-09-13 22:04:51 +0200

    🔗 added middleware redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 /* eslint-disable import/prefer-default-export */
+import axios from 'axios';
+
 export const removeItem = (itemType, id) => {
   return {
     type: 'REMOVE_ITEM',
@@ -25,4 +27,22 @@ export const addItem = (itemType, itemContent) => {
       },
     },
   };
+};
+
+export const authenticate = (username, password) => dispatch => {
+  dispatch({ type: 'AUTHENTICATE_REQUEST' });
+
+  return axios
+    .post('http://localhost:9000/api/user/login', {
+      username,
+      password,
+    })
+    .then(payload => {
+      console.log(payload);
+      dispatch({ type: 'AUTHENTICATE_SUCCESS', payload });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: 'AUTHENTICATE_FAILURE' });
+    });
 };
