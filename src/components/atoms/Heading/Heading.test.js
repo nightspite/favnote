40a0b704a748e1dde27f7158878b580578412c63
nightspite <n16h75p173@gmail.com>
2020-09-17 21:05:26 +0200

    🧪 prepared for testing

---
+++ src/components/atoms/Heading/Heading.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/mainTheme';
+import Heading from 'components/atoms/Heading/Heading';
+
+describe('Heading Component', () => {
+  it('Renders children text', () => {
+    const { getByText } = render(
+      <ThemeProvider theme={theme}>
+        <Heading>Hello</Heading>
+      </ThemeProvider>,
+    );
+
+    getByText('Hello Roman');
+  });
+});
